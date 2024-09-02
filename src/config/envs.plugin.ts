15de5640c8276e09f565b80ef30a4c551b67f3f0
Oziel Lemus <114196082+OzielLM@users.noreply.github.com>
2024-09-02 16:39:20 -0600

    Create email service

--- src/config/envs.plugin.ts
+++ src/config/envs.plugin.ts
@@ -3,5 +3,8 @@ import * as env from 'env-var'
 
 export const envs = {
     PORT: env.get("PORT").asPortNumber(),
-    MONGO_URL: env.get("MONGO_URL").asString()
+    MONGO_URL: env.get("MONGO_URL").asString(),
+    MAIL_SERVICE: env.get("MAIL_SERVICE").asString(),
+    MAIL_SECRET_KEY: env.get("MAIL_SECRET_KEY").asString(),
+    MAIL_USER: env.get("MAIL_USER").asString()
 }
